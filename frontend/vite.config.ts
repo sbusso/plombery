9f20558d1fb28e2b0c625c56ab020ecdc869c039
Stéphane Busso <stephane.busso@gmail.com>
2025-01-07 10:05:44 +1300

    remove manual chunk

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,19 +12,19 @@ export default defineConfig({
   plugins: [react()],
   build: {
     sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (id.includes('node_modules')) {
-            if (id.includes('handsontable')) return 'table'
-            if (id.includes('react')) return 'vendor'
-            if (id.includes('@tremor') || id.includes('@headlessui')) return 'ui'
-            if (id.includes('@tanstack') || id.includes('socket.io')) return 'data'
-            return 'deps'
-          }
-        },
-      },
-    },
+    // rollupOptions: {
+    //   output: {
+    //     manualChunks: (id) => {
+    //       if (id.includes('node_modules')) {
+    //         if (id.includes('handsontable')) return 'table'
+    //         if (id.includes('react')) return 'vendor'
+    //         if (id.includes('@tremor') || id.includes('@headlessui')) return 'ui'
+    //         if (id.includes('@tanstack') || id.includes('socket.io')) return 'data'
+    //         return 'deps'
+    //       }
+    //     },
+    //   },
+    // },
     chunkSizeWarningLimit: 500,
   },
 })
